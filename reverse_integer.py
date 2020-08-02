08bb488b454fe4d37ce1493a140fc4de6544f3b8
globalfranck <50407517+globalfranck@users.noreply.github.com>
2020-08-02 12:17:40 +0200

    Adding test cases

--- reverse_integer.py
+++ reverse_integer.py
@@ -1,3 +1,22 @@
 # Input = positive or negative integer number
 # Output = reversed integer
 
+def reverse_int (int_num):
+	""" function takes an integer and return it reversed """
+	return int_num
+
+
+# Unit test cases
+num1 = 123
+num2 = -987
+num3 = 0
+num4 = "12"
+num5 = None
+
+# Showcasing
+
+print(f"The reversed version of : {num1} is {reverse_int(num1)}")
+print(f"The reversed version of : {num2} is {reverse_int(num2)}")
+print(f"The reversed version of : {num3} is {reverse_int(num3)}")
+print(f"The reversed version of : {num4} is {reverse_int(num4)}")
+print(f"The reversed version of : {num5} is {reverse_int(num5)}")
